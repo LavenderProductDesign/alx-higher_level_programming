7fbac3090bd51c3056254713a960aa2473c04add
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-06-06 10:17:05 +0300

    print list changes

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-#function that prints all integers of a list
-#prototype def print_list_integer(my_list=[]):
-#use str.format()
+# function that prints all integers of a list
+# prototype def print_list_integer(my_list=[]):
+# use str.format()
+
 
 def print_list_integer(my_list=[]):
     for i in my_list:
