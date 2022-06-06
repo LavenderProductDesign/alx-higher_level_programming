668c4c8e5c0fa065a7ca3bf748b9cdb1a015b9c1
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-06-06 11:44:03 +0300

    retrieves element changes

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,12 +1,8 @@
 #!usr/bin/python3
-# function replaces element of a list at a specific position
-# prototype def replace_in_list(my_list, idx, element):
-
-
 def element_at(my_list, idx):
     if idx < 0:
         return None
     elif idx >= len(my_list):
         return None
     else:
-        return (my_list[idx])
+        return my_list[idx]
