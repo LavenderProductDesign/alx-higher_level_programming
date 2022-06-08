47dfaa640e61e675caef4ae8cd62c4afd7a1baca
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-06-08 17:01:27 +0300

    roman edited

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                'C': 100, 'D': 500, 'M': 1000}
-    if type(roman_string) is not str or roman_string is None:
+    table = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    strng = list(roman_string[::-1].upper())
-    flag = 1
-    sum = 0
-    for l in range(len(strng) - 1):
-        sum = sum + numerals[strng[l]]*flag
-        if numerals[strng[l]] <= numerals[strng[l + 1]]:
-            flag = 1
-        if numerals[strng[l]] > numerals[strng[l + 1]]:
-            flag = -1
-    if flag == 1:
-        sum = sum + numerals[strng[len(strng) - 1]]
-    if flag == -1:
-        sum = sum - numerals[strng[len(strng) - 1]]
+    prev, sum = 0, 0
+
+    for a in roman_string:
+        sum += table[a] if table[a] <= prev else table[a] - prev * 2
+        prev = table[a]
     return sum
