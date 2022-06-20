3fe0c2941f4ecbf13ae8ccdb07229063eca0be70
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-06-20 22:52:23 +0300

    print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
-            count = count + 1
-        except(NameError, IndexError, ValueError):
+            count += 1
+        except (ValueError, TypeError, IndexError):
             break
     print()
-    return 
+    return count
