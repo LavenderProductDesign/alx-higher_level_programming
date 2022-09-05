b07f6eae29001a7fba82ae99baa58ea1ccc65afb
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-09-05 16:59:10 +0300

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 let nextMax = 0;
-const args = process.argv.slice(2);
+const args = process.argv.slice(2).map((x) => {
+  return parseInt(x);
+});
+
 if (args.length > 1) {
   args.sort();
   nextMax = args[args.length - 2];
